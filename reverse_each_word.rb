2b78674fe23ba83b3e9cb2fe20f117cff1057341
Jonas Kavaliauskas <jonas.kavaliauskas@yale.edu>
2020-05-22 11:38:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,4 +11,14 @@ def reverse_each_word(string)
 end
 
 
-def
+def reverse_each_word(string)
+ string = string.split(" ")
+ reversed_string = []
+ 
+ string.each do |word|
+  reversed_string << word.reverse
+ end
+ 
+ 
+ reversed_string.join(" ")
+end
